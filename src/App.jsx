79c797e89abdb9a3b45e79224ce99f8d03bf6f56
rebelcl0ud <y0j0v33@gmail.com>
2024-01-29 20:30:52 -0600

    update: using jsx goodness

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement(
-    "div",
-    {}, // this is for any attributes you'd like to add; if keeping empty {} or null work
-    [
-      React.createElement("h1", {}, "Adopt Me!"),
-      React.createElement(Pet, {
-        name: "Luna",
-        animal: "Dog",
-        breed: "Havanese",
-      }),
-      React.createElement(Pet, {
-        name: "Bandit",
-        animal: "Dog",
-        breed: "Mixed",
-      }),
-      React.createElement(Pet, {
-        name: "Flavor",
-        animal: "Dog",
-        breed: "Mixed",
-      }),
-    ]
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese" />
+      <Pet name="Bandit" animal="Dog" breed="Mixed" />
+      <Pet name="Flavor" animal="Dog" breed="Mixed" />
+    </div>
   );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
