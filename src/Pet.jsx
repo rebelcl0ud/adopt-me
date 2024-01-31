44ef30acd31ff7867602e83979c1e3e710111f97
rebelcl0ud <y0j0v33@gmail.com>
2024-01-31 14:45:14 -0600

    update: style Pet component, add missing props to Results component

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,10 +1,20 @@
 const Pet = (props) => {
+  const { id, name, animal, breed, images, location } = props;
+
+  let heroImg = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    heroImg = images[0];
+  }
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={heroImg} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 };
 
--- src/Results.jsx
+++ src/Results.jsx
@@ -13,6 +13,8 @@ const Results = ({ pets }) => {
               name={pet.name}
               animal={pet.animal}
               breed={pet.breed}
+              images={pet.images}
+              location={`${pet.city}, ${pet.state}`}
             />
           );
         })
