f8f7ef07d45e59b6107ee13f537adea2f95599fa
rebelcl0ud <y0j0v33@gmail.com>
2024-02-03 14:52:36 -0600

    remove: cleanup console.log and stray curly brace

--- src/Details.jsx
+++ src/Details.jsx
@@ -14,14 +14,13 @@ const Details = () => {
     );
   }
 
-  console.log({ results });
   const pet = results.data.pets[0];
 
   return (
     <div className="details">
       <div>
         <h1>{pet.name}</h1>
-        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state} }`}</h2>
+        <h2>{`${pet.animal} - ${pet.breed} - ${pet.city}, ${pet.state}`}</h2>
         <button>Adopt {pet.name}</button>
         <p>{pet.description}</p>
       </div>
